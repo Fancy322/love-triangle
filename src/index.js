a86a3a983c78de9191682a551cb523c7ccdb7b0c
Fancy322 <vav10041997@gmail.com>
2018-09-18 23:05:30 +0300

    Commit triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  var p1, p2, c = 0;
+  
+  for(var i = 0; i < preferences.length; i++)
+  {
+    p1 = preferences[i];
+    p2 = preferences[p1 - 1];
+       if(preferences[p2 - 1] == i + 1) 
+        c++;
+   }
+  return Math.floor(c / 3);
   // your implementation
 };
